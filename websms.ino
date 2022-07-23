c18f26b8109fbc2077aca4c8678ea2ac5cb53655
israfilgazi <md.israfilgazi@gmail.com>
2022-07-23 17:55:39 +0600

    Update websms.ino

--- websms.ino
+++ websms.ino
@@ -21,28 +21,24 @@ time  04/01/01,00:05:15+32"
 #include <EEPROM.h>
 #include <WiFiClientSecure.h>
 /*==========================*/
-//#include <AsyncTCP.h>
+#include <WiFiManager.h>
 #include <DNSServer.h>
 #include <ArduinoJson.h>
 /*============================*/
 #include <SoftwareSerial.h>
 /*ADD YOUR PASSWORD BELOW*/
+//const char *password = "";
 String ssid     = "";
 String password = "";
 String apiurl   = "";
 String apikey   = "";
-String content  = "";
-String st       = "";
-int statusCode  = 0;
 int value;
 /*eprom------------*/
 void apirequst(void);
 void defalut(void);
 bool testWifi(void);
-void createWebServer(void);
-void setupAP(void);
-void scanAP(void);
 void readeprom(void);
+void writeprom(void);
 
 String sendsms(String num,String msg);
 void  gsmsetup(void);
@@ -52,7 +48,7 @@ uint8_t KEY_RST   = D1; // declare LED pin on NodeMCU Dev Kit
 uint8_t LED_PIN   = D0; // declare LED pin on NodeMCU Dev Kit
 
 SoftwareSerial gsm(D5,D6);//  Gnd, D5-TX, D6-RX
-ESP8266WebServer server(80);
+WiFiManager wifimanager;
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
@@ -64,27 +60,47 @@ void setup() {
     EEPROM.begin(512); 
     delay(500);
     readeprom();
-    WiFi.mode(WIFI_AP);
+    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);// Connect to Wi-Fi
     servermode = false;
     if (digitalRead(KEY_RST) == LOW || !testWifi()){
-        setupAP();
-        scanAP();
-        createWebServer();
-        servermode = true;   
+        digitalWrite(LED_PIN, LOW);
+        WiFi.mode(WIFI_AP); 
+        const char *url = apiurl.c_str();
+        const char *key = apikey.c_str();
+        WiFiManagerParameter custom_text_box("apiurl", "Api url",url, 50);
+        WiFiManagerParameter custom_text_box_num("apikey", "api number count(7)", key, 7);    
+        // Add custom parameter
+        wifimanager.addParameter(&custom_text_box);
+        wifimanager.addParameter(&custom_text_box_num);
+        bool res;
+        res = wifimanager.autoConnect("websms",""); // password protected ap   
+        if(!res)Serial.println("Failed to connect");// ESP.restart();
+        else Serial.println("connected...yeey :)"); //if you get here you have connected to the WiFi    
+        digitalWrite(LED_BUILTIN, HIGH);
+        WiFi.persistent(true);
+        WiFi.setAutoConnect(true);
+        WiFi.setAutoReconnect(true);
+        
+        ssid = WiFi.SSID();
+        password = WiFi.psk();
+        apiurl = custom_text_box.getValue();
+        apikey = custom_text_box_num.getValue();
+        Serial.println("READ EPROM ssid:"+ssid+",password:"+password+",apiurl:"+apiurl+",apikey:"+apikey);
+        writeprom();
+        servermode = true; 
+        wifimanager.resetSettings();
+        ESP.reset(); 
     } 
 //===========================
   if(!servermode){
     gsmsetup(); 
     WiFi.mode(WIFI_STA);
-    WiFi.persistent(true);
-    WiFi.setAutoConnect(true);
-    WiFi.setAutoReconnect(true);
   }
 }
 
 void loop(){
-    if(digitalRead(KEY_RST) == LOW && !servermode){ 
+    if(digitalRead(KEY_RST) == LOW){ 
           defalut();          
     }
     if (!servermode && WiFi.status() == WL_CONNECTED){
@@ -93,49 +109,19 @@ void loop(){
           digitalWrite(LED_PIN, LOW);
           delay(1000);
     }
-    
     if(!servermode && WiFi.status() != WL_CONNECTED){
       digitalWrite(LED_PIN, LOW);
       delay(500);
       digitalWrite(LED_PIN, HIGH);
       delay(500);
     }
-    
-    if(servermode){
-        server.handleClient(); //this is required for handling the incoming requests
-        //Serial.println("handleClient");
-    }
 }
 
-void readeprom() {
-    ssid = ""; password = ""; apiurl = ""; apikey = "";
-    for (int i = 0; i < 32; ++i)
-    {
-      value = EEPROM.read(i);
-      if(value != '\0')ssid += (char)(value);
-    }
-    for (int i = 32; i < 64; ++i)
-    {
-      value = EEPROM.read(i);
-      if(value != '\0')password += (char)(value);
-    }
-    for (int i = 64; i < 96; ++i)
-    {
-      value = EEPROM.read(i);
-      if(value != '\0')apikey += (char)(value);
-    }
-    for (int i = 96; i < 160; ++i)
-    {
-      value = EEPROM.read(i);
-      if(value != '\0')apiurl += (char)(value);
-    }
-    
-    Serial.println("READ EPROM ssid:"+ssid+",password:"+password+",apiurl:"+apiurl+",apikey:"+apikey);
-}
 void defalut() {
     digitalWrite(LED_PIN, LOW);
     Serial.println("factory defalut");
     WiFi.disconnect();
+    wifimanager.resetSettings();
     for (int i = 0 ; i < EEPROM.length() ; i++) {
       EEPROM.write(i, 0);
     }
@@ -196,7 +182,16 @@ void apirequst() {
            }
         }
      }else {
+         digitalWrite(LED_PIN, LOW);
+         delay(10);
          Serial.printf("[HTTPS] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+         digitalWrite(LED_PIN, HIGH);
+         delay(10);
+         digitalWrite(LED_PIN, LOW);
+         delay(10);
+         digitalWrite(LED_PIN, HIGH);
+         digitalWrite(LED_PIN, LOW);
+         delay(10);
      }
      http.end();
      delay(1500);
@@ -276,140 +271,59 @@ bool testWifi(void)
     Serial.print("*");
     c++;
   }
-  Serial.println("");
   Serial.println("Connect timed out, opening AP");
   return false;
 }
 
-void setupAP(void)
+void readeprom() {
+    ssid = ""; password = ""; apiurl = ""; apikey = "";
+    for (int i = 0; i < 32; ++i)
+    {
+      value = EEPROM.read(i);
+      if(value != '\0')ssid += (char)(value);
+    }
+    for (int i = 32; i < 64; ++i)
+    {
+      value = EEPROM.read(i);
+      if(value != '\0')password += (char)(value);
+    }
+    for (int i = 64; i < 96; ++i)
+    {
+      value = EEPROM.read(i);
+      if(value != '\0')apikey += (char)(value);
+    }
+    for (int i = 96; i < 160; ++i)
+    {
+      value = EEPROM.read(i);
+      if(value != '\0')apiurl += (char)(value);
+    }   
+    Serial.println("READ EPROM ssid:"+ssid+",password:"+password+",apiurl:"+apiurl+",apikey:"+apikey);
+}
+
+void writeprom()
 {
-  WiFi.disconnect();
-  WiFi.mode(WIFI_STA);
-  
-  delay(100);
-    
-  IPAddress local_IP(192,168,1,1);
-  IPAddress gateway(192,168,1,10);
-  IPAddress subnet(255,255,255,0);
-
-  Serial.print("Setting soft-AP configuration ... ");
-  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-  Serial.println(WiFi.softAP("websms") ? "Ready" : "Failed!");
-  Serial.print("Soft-AP IP address = ");
-  Serial.println(WiFi.localIP());
-  Serial.println(WiFi.softAPIP());
-  Serial.println("Initializing_softap_for_wifi credentials_modification");
-  Serial.println("over");
-}
-void scanAP(){
-  delay(100);
-  int n = WiFi.scanNetworks();
-  st = "<ol>";
-  for (int i = 0; i < n; ++i)
-  {
-    // Print SSID and RSSI for each network found
-    st += "<li>";
-    st += WiFi.SSID(i);
-    st += " (";
-    st += WiFi.RSSI(i);
-    st += ")";
-    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
-    st += "</li>";
-  }
-  st += "</ol>";
-  delay(100); 
-}
-
-void createWebServer()
-{  
-    server.on("/", []() {
-      //Serial.println(WiFi.localIP());
-      //Serial.println(WiFi.softAPIP());
-      //IPAddress ip = WiFi.softAPIP();
-      //String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
-      content = "<!DOCTYPE HTML>\r\n<html><head><meta name='viewport' content='width=device-width, initial-scale=1'><style>";
-      content += ".w-100{width:100%;display: inline-block;} body{font-family: Arial, Helvetica, sans-serif;} form{border: 3px solid #f1f1f1;}";
-      content += "input[type=text], input[type=password] {width: 100%;padding: 12px 20px;margin: 8px 0;display: inline-block;border: 1px solid #ccc;box-sizing: border-box;}";
-      content += "input[type=submit] {background-color: #04AA6D;color: white;padding: 14px 20px;margin: 8px 0;border: none;cursor: pointer;width: 100%;}";
-      content += "</style></head><body class='w-100'>Welcome to Wifi Credentials Update page";
-      content += "<form action=\"/scan\" method=\"post\"><input type=\"submit\" value=\"scan\"></form>";
-      content += "<form action=\"/restart\" method=\"post\"><input type=\"submit\" value=\"Restart\"></form>";
-      //content += ipStr;
-      content += "<p>";
-      content += st;
-      content += "</p><form method='get' action='setting'><p><label>SSID:   </label><input name='ssid' value='"+ssid+"' length=32></p><p><label>Password: </label><input name='pass' value='"+password+"' length=32></p><p><label>ApiUrl:  </label><input name='url' value='"+apiurl+"' length=64></p><p><label>Apikey:   </label><input name='key' value='"+apikey+"' length=32><input type='submit'></p></form>";
-      content += "</body></html>";
-      server.send(200, "text/html", content);
-    });
-    
-    server.on("/scan", []() {
-      scanAP();
-      //IPAddress ip = WiFi.softAPIP();
-      //String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
-      //content = "<!DOCTYPE HTML>\r\n<html>go back";
-      //server.send(200, "text/html", content);
-      server.sendHeader("Location", String("/"), true);
-      server.send ( 302, "text/plain", "");
-    });
-    
-    server.on("/restart", []() {
-        server.sendHeader("Location", String("/"), true);
-        server.send ( 302, "text/plain", "");
-        delay(5000);
-        ESP.reset();
-    });
-    
-    server.on("/setting", []() {
-      String qsid = server.arg("ssid");
-      String qpass = server.arg("pass");
-      String qpurl = server.arg("url");
-      String qpkey = server.arg("key");
-      if (qsid.length() > 0 && qpurl.length() > 0 && qpkey.length() > 0) {
-        Serial.println("clearing eeprom");
-        for (int i = 0; i < 512; ++i) {
+   for (int i = 0; i < 512; ++i) {
           EEPROM.write(i, 0);
-        }
-        Serial.println("writing eeprom.. ssid:"+qsid+",password:"+qpass+",apiurl:"+qpurl+",apikey:"+qpkey);
-        
-        for (int i = 0; i < qsid.length(); ++i)
-        {
-          EEPROM.write(i, qsid[i]);
-        }
-        
-        for (int i = 0; i < qpass.length(); ++i)
-        {
-          EEPROM.write(32 + i, qpass[i]);
-        }
-
-        for (int i = 0; i < qpkey.length(); ++i)
-        {
-          EEPROM.write(64 + i, qpkey[i]);
-        }
-        
-        for (int i = 0; i < qpurl.length(); ++i)
-        {
-          EEPROM.write(96 + i, qpurl[i]);
-        }
-        qsid = ""; qpass = ""; qpurl = ""; qpkey = "";
-        EEPROM.commit();
-        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
-        statusCode = 200;
-        //server.sendHeader("Access-Control-Allow-Origin", "*");
-        //server.send(statusCode, "application/json", content);
-        readeprom();
-        server.sendHeader("Location", String("/"), true);
-        server.send ( 302, "text/plain", "");
-      }else {
-        content = "{\"Error\":\"404 not found\"}";
-        statusCode = 404;
-        Serial.println("Sending 404");
-        server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(statusCode, "application/json", content);
-      }
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-      server.send(statusCode, "application/json", content);
-    });
-  
-  server.begin();// Start server
-  Serial.print("Start server:");
+   }
+   
+   for (int i = 0; i < ssid.length(); ++i)
+   {
+      EEPROM.write(i, ssid[i]);
+   }
+   
+   for (int i = 0; i < password.length(); ++i)
+   {
+      EEPROM.write(32 + i, password[i]);
+   }
+   
+    for (int i = 0; i < apikey.length(); ++i)
+    {
+       EEPROM.write(64 + i, apikey[i]);
+    }
+    for (int i = 0; i < apiurl.length(); ++i)
+    {
+       EEPROM.write(96 + i, apiurl[i]);
+    }
+    EEPROM.commit();
+    Serial.print("writing eeprom DONE:");
 }
